c34a4e8445885110d655a859d6ed19685106bcb4
tiffany_k <tiffany.kantz@gmail.com>
2023-12-07 15:55:05 -0500

    Added functionality to make cat appear and disappear

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 // console.log(cat);
 
-// show cat on click
+// show or remove cat on click
 button.addEventListener("click", function() {
-    // console.log("It's working!");
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        // console.log("Yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        // console.log("No");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!!";
+        button.classList.remove("disappear");
+    }
 });
